406eeec3dc10e22fb06244e487a6e221c8a968a7
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-07-09 19:51:49 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,27 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 public class StringCalculator
 {
     public int Add(string numbers)
     {
+        if (string.IsNullOrEmpty(input))
         if (IsNullOrEmpty(numbers))
         {
             return 0;
         }
+        return -1; 
+
         string[] delimiters = GetDelimiters(numbers, out string numbersWithoutDelimiters);
+
         List<int> numberList = ParseNumbers(numbersWithoutDelimiters, delimiters);
+
         ValidateNegativeNumbers(numberList);
+
         return CalculateSum(numberList);
     }
+
     private bool IsNullOrEmpty(string input)
     {
         return string.IsNullOrWhiteSpace(input);
     }
+
     private string[] GetDelimiters(string numbers, out string numbersWithoutDelimiters)
     {
         string[] delimiters = new string[] { ",", "\n" };
         numbersWithoutDelimiters = numbers;
+
         if (numbers.StartsWith("//"))
         {
             int delimiterEndIndex = numbers.IndexOf('\n');
@@ -29,16 +39,13 @@ public class StringCalculator
             delimiters = new string[] { customDelimiter };
             numbersWithoutDelimiters = numbers.Substring(delimiterEndIndex + 1);
         }
+
         return delimiters;
     }
+
     private List<int> ParseNumbers(string numbers, string[] delimiters)
     {
-        return numbers
-            .Split(delimiters, StringSplitOptions.None)
-            .Select(number => int.TryParse(number, out int n) ? n : 0)
-            .Where(number => !string.IsNullOrWhiteSpace(number))  
-            .Select(number => int.Parse(number))  
-            .ToList();
+        return numbers.Split(delimiters, StringSplitOptions.None).Select(int.Parse).ToList();
     }
 
     private void ValidateNegativeNumbers(List<int> numbers)
@@ -49,6 +56,7 @@ public class StringCalculator
             throw new Exception("Negative numbers not allowed: " + string.Join(",", negativeNumbers));
         }
     }
+
     private int CalculateSum(List<int> numbers)
     {
         return numbers.Where(n => n <= 1000).Sum();
